97adabc683e1333d1350e9b40dadc1cdc28f6b33
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2022-04-16 23:03:50 +0300

    Update 100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,50 +1,60 @@
-
+#include "holberton.h"
 /**
- * print_times_table - prints times table
- * @n : times table to use
- * Return:void
+ *print_times_table - prints timestable from n
+ *
+ *@n:input
+ *
+ *Return: void
+ *
  */
-
-void times_table(int n)
+void print_times_table(int n)
 {
+	int result;
 
-int a = 0, rep, b;
-
-if (n < 0 || n > 15)
-	return;
-
-while (a <= n)
-{
-	for (b = 0; b <= n; b++)
+	if (n <= 15 && n > 0)
 	{
-		rep = a * b;
-		if (b == 0)
-			_putchar('0' + rep);
-		else if (rep < 10)
+		int i;
+
+		for (i = 0; i <= n; i++)
 		{
-			_putchar(' ');
-			_putchar(' ');
-			_putchar('0' + rep);
-		}
-		else if (rep < 100)
-		{
-			_putchar(' ');
-			_putchar('0' + rep / 10);
-			_putchar('0' + rep % 10);
-		}
-		else
-		{
-			_putchar('0' + rep / 100);
-			_putchar('0' + (rep - 100) / 10);
-			_putchar('0' + rep % 10);
-		}
-		if (b < n)
-		{
-			_putchar(',');
-			_putchar(' ');
+			int j;
+
+			for (j = 0; j <= n; j++)
+			{
+				result = i * j;
+				if (j == 0)
+				{
+					_putchar('0' + result);
+				}
+				else if (result >= 10)
+				{
+					_putchar(' ');
+					_putchar('0' + (result / 10));
+					_putchar('0' + (result % 10));
+				}
+				else if (result >= 100)
+				{
+					_putchar(' ');
+					_putchar('0' + (result / 100));
+					_putchar('0' + ((result / 10) % 10));
+					_putchar('0' + (result % 10));
+				}
+				else
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(result + '0');
+				}
+				if (j != n)
+				{
+					_putchar(',');
+				}
+			}
+			_putchar('\n');
 		}
 	}
-	_putchar('\n');
-	a++;
-}
+	else
+	{
+		_putchar('\n');
+	}
 }
