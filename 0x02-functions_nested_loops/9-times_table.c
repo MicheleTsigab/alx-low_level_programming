b95cab654e704f70f60b3c507f95c298edc14524
MicheleTsigab <54311559+MicheleTsigab@users.noreply.github.com>
2022-04-16 22:56:22 +0300

    improved time table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,47 +1,51 @@
-#include "main.h"
+#include "holberton.h"
+
 /**
- * times_table -prints tables
- *
- * Return: Always 0.
+ * print_times_table - prints times table
+ * @n : times table to use
+ * Return:void
  */
-void times_table(void)
+
+void print_times_table(int n)
 {
-int a;
-int b;
-int c;
-for (a = 0; a <= 9; a++)
+
+int a = 0, rep, b;
+
+if (n < 0 || n > 15)
+	return;
+
+while (a <= n)
 {
-for (b = 0; b <= 9; b++)
-{
-c = a * b;
-if ((c / 10) == 0)
-{
-if (b == 0)
-{
-_putchar ('0');
-}
-if (b != 0)
-{
-_putchar (' ');
-_putchar ((c % 10) + '0');
-}
-if (b < 9)
-{
-_putchar(',');
-_putchar (' ');
-}
-}
-else
-{
-_putchar ((c / 10) + '0');
-_putchar ((c % 10) + '0');
-if (b < 9)
-{
-_putchar(',');
-_putchar (' ');
-}
-}
-}
-_putchar ('\n');
+	for (b = 0; b <= n; b++)
+	{
+		rep = a * b;
+		if (b == 0)
+			_putchar('0' + rep);
+		else if (rep < 10)
+		{
+			_putchar(' ');
+			_putchar(' ');
+			_putchar('0' + rep);
+		}
+		else if (rep < 100)
+		{
+			_putchar(' ');
+			_putchar('0' + rep / 10);
+			_putchar('0' + rep % 10);
+		}
+		else
+		{
+			_putchar('0' + rep / 100);
+			_putchar('0' + (rep - 100) / 10);
+			_putchar('0' + rep % 10);
+		}
+		if (b < n)
+		{
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar('\n');
+	a++;
 }
 }
